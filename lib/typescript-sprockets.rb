a3373e47143d38f6e1d01e7acda261e49ecbd87f
Preetpal Sohal <preetpal.sohal@gmail.com>
2016-08-20 12:56:37 -0700

    Non-qualified reference bug fix.

--- lib/typescript-sprockets.rb
+++ lib/typescript-sprockets.rb
@@ -3,4 +3,4 @@ require 'typescript/sprockets/typescript_processor'
 require 'typescript/sprockets/version'
 
 ::Sprockets.register_mime_type 'text/typescript', extensions: ['.js.ts']
-::Sprockets.register_transformer 'text/typescript', 'application/javascript', TypescriptProcessor
+::Sprockets.register_transformer 'text/typescript', 'application/javascript', Typescript::Sprockets::TypescriptProcessor
